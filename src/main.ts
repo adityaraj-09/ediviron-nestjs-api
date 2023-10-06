9ae4026124055d72a23a92805b19c6ed2d7fcbf7
adityaraj-09 <itsrishabhforyou@gmail.com>
2023-10-07 03:31:39 +0530

    b

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import  express from 'express';
 
 async function bootstrap() {
   const server = express();
